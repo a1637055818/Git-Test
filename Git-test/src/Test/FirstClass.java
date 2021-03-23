d84b2646a30c2f17366bbb4e940b0f8d02d47f80
zhifei_glb <zhifei_global@qq.com>
2021-03-23 23:05:35 +0800

    把本地工程添加到git

---
+++ Git-test/src/Test/FirstClass.java
@@ -0,0 +1,13 @@
+package Test;
+
+/**
+ * @author 16370
+ * @create 2021-03-23 下午 10:55
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+
+}
